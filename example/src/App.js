0ef04400599057ce7c44ce4a53490e027eeb6f48
Mustafa <mustafa.mamun.khondkar@ericsson.com>
2020-03-29 21:42:45 +0300

    example updated

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import ExampleComponent from 'react-calendar'
-
-const App = () => {
+import Calendar from 'react-calendar-basic'
+import 'react-calendar-basic/dist/style/styles.css'
+function App() {
+  const onClickedEvent = () => {
+    console.log('onClickedEvent')
+  }
+  const onSelect = () => {
+    console.log('onSelect')
+  }
+  const onNavigation = () => {}
+  const onViewChange = () => {}
+  const events = [
+    {
+      start: new Date('2020-03-29T19:50:00+03:00'),
+      end: new Date('2020-03-29T20:10:00+03:00'),
+      title: 'This is a event on mac sdsdfg asdfas asdfsa asfa asdfa asdfasdf'
+    }
+  ]
   return (
-    <div>
-      <ExampleComponent text='Modern React component module' />
+    <div className='App'>
+      <Calendar
+        onNavigation={onNavigation}
+        onViewChange={onViewChange}
+        events={events}
+        onSelect={onSelect}
+        onClickedEvent={onClickedEvent}
+      />
     </div>
   )
 }
